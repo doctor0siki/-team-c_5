c8751dd7ac84a2b0c2805ace57b8cea4a9383cfb
nt16145 <nt16145@tomakomai.kosen-ac.jp>
2019-09-19 17:15:00 +0900

    join_group

--- app/Controller/join_group/index.php
+++ app/Controller/join_group/index.php
@@ -21,6 +21,6 @@ $app->post('/join_group/', function (Request $request, Response $response) {
     //ユーザーDAOをインスタンス化
     $group = new Group($this->db);
 
-    $result = $group->select($data,"","",1,false)
+    $result = $group->select($data,"","",1,false);
 
 });
