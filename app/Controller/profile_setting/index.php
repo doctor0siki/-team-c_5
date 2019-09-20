b9057551f49907b5692223e2dd330ed72c5b8770
Bokoko <kamabokoman1042@gmail.com>
2019-09-20 13:30:39 +0900

    設定後画面作成

--- app/Controller/profile_setting/index.php
+++ app/Controller/profile_setting/index.php
@@ -26,7 +26,7 @@ $app->post('/profile_setting/', function (Request $request, Response $response) 
     $group = new User($this->db);
 
     //DBに登録をする。戻り値は自動発番されたIDが返ってきます
-    $id = $group->insert($data);
+    $id = $group->update($data);
 
     return $this->view->render($response, 'profile_setting/done.twig', $data);
 
